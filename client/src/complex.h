853baa99e8491f94f95cacc83e187cb14036de02
gnksnm <maximovegor56@yandex.ru>
2025-11-24 00:51:02 +0300

    Delete client directory

--- client/src/complex.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <QString>
-
-struct Complex {
-    double re{0.0}, im{0.0};
-    Complex() = default;
-    Complex(double r, double i): re(r), im(i) {}
-    QString toString() const { return QString::number(re) + (im>=0?"+":"") + QString::number(im) + "i"; }
-};
